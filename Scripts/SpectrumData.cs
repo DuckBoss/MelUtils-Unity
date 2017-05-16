5eca6f71e550097aafbf717d171c778022c07d31
Jason <DuckBoss@users.noreply.github.com>
2017-05-16 19:24:18 -0400

    Update SpectrumData.cs

--- Scripts/SpectrumData.cs
+++ Scripts/SpectrumData.cs
@@ -1,20 +1,21 @@
 using UnityEngine;
 
-public class SpectrumData {
-    public float[] GetSpectrum(AudioSource audioSource, float[] samples) {
-        audioSource.GetSpectrumData(samples, 0, channelWindow);
-        return samples;
-    }
-    
-    public IEnumerator LoadSongFromPath(string path, AudioSource audioSource) {
-		WWW www = new WWW("file:///" + path);
-		while(!www.isDone) {
+namespace JJAudioUtils {
+	public class SpectrumData {
+	    public float[] GetSpectrum(AudioSource audioSource, float[] samples) {
+		audioSource.GetSpectrumData(samples, 0, channelWindow);
+		return samples;
+	    }
+
+	    public IEnumerator LoadSongFromPath(string path, AudioSource audioSource) {
+			WWW www = new WWW("file:///" + path);
+			while(!www.isDone) {
+				yield return null;
+			}
+
+			audioSource.clip = www.GetAudioClip(false,true);
 			yield return null;
-		}
-
-		audioSource.clip = www.GetAudioClip(false,true);
-		yield return null;
-    }
+	    }
+	}
 }
-
 
