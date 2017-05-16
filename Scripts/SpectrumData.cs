aa697f43d566885cf2d7eb4f07f7ab85161f19d1
Jason <DuckBoss@users.noreply.github.com>
2017-05-16 19:22:48 -0400

    Update SpectrumData.cs

--- Scripts/SpectrumData.cs
+++ Scripts/SpectrumData.cs
@@ -1,8 +1,20 @@
 using UnityEngine;
 
 public class SpectrumData {
-    private float[] GetSpectrum(AudioSource audioSource, float[] samples) {
+    public float[] GetSpectrum(AudioSource audioSource, float[] samples) {
         audioSource.GetSpectrumData(samples, 0, channelWindow);
         return samples;
     }
+    
+    public IEnumerator LoadSongFromPath(string path, AudioSource audioSource) {
+		WWW www = new WWW("file:///" + path);
+		while(!www.isDone) {
+			yield return null;
+		}
+
+		audioSource.clip = www.GetAudioClip(false,true);
+		yield return null;
+    }
 }
+
+
